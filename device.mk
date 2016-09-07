58b07bb03dc139f9bd9c71dfe7e61d94c4b46992
Adriano Martins <adrianomartins@gmail.com>
2016-09-07 00:37:48 -0300

    Revert "libcamera_client_mtk: build from source"
    This reverts commit 9f42d78ae89832028db636539632037dc9829636.

--- device.mk
+++ device.mk
@@ -283,7 +283,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
 	external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:system/etc/permissions/com.dsi.ant.antradio_library.xml
 
-# Camera
-PRODUCT_PACKAGES += \
-      libcamera_client_mtk
-
